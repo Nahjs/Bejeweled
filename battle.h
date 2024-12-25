2f360b79d488a232668cbc6516d482a41504635e
Nahjs <23301132@bjtu.edu.cn>
2024-12-25 17:04:46 +0800

    对战模式ui类

---
+++ battle.h
@@ -0,0 +1,51 @@
+#ifndef BATTLE_H
+#define BATTLE_H
+
+#include <QMainWindow>
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QScrollArea>
+#include <QPushButton>
+#include "battlegame.h"
+#include "Client/chatclient.h"
+#include "login.h"  // 添加 login.h 头文件
+
+namespace Ui {
+class Battle;
+}
+
+class Battle : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit Battle(ChatClient* client, QWidget *parent = nullptr);
+    ~Battle();
+
+private slots:
+    void onBackButtonClicked();
+    void updateOpponentDisplays(const QStringList& players);
+
+    void handleMessage(const QString &type, const QString &data);
+
+    void handleNewPlayer(const QString& playerId, const QString& playerName);
+    void handlePlayerLeft(const QString& playerId);
+
+signals:
+    void battleToStart();  // 返回开始界面的信号
+
+private:
+    Ui::Battle *ui;
+    ChatClient* m_client;
+    BattleGame* m_playerGame;
+    QMap<QString, BattleGame*> m_opponentGames;  // 键改为用户名而不是UUID
+    QMap<QString, QString> m_opponentIds;        // 用户名到UUID的映射
+    QLayout* m_opponentsLayout;  // 改为QLayout*以支持不同类型的布局
+    
+    void setupUI();
+    void resizeOpponentDisplays();
+    void clearOpponentDisplays();
+    int calculateOptimalcolumns();
+};
+
+#endif // BATTLE_H
